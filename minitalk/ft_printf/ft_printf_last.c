179e999e4c7275c842bf062745cbf088dd0c9bf8
Supinkusu001 <116731966+Supinkusu001@users.noreply.github.com>
2022-12-30 01:48:57 +0100

    Add files via upload

---
+++ minitalk/ft_printf/ft_printf_last.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_last.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aennaouh <aennaouh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/19 18:36:56 by aennaouh          #+#    #+#             */
+/*   Updated: 2022/11/20 19:58:57 by aennaouh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_puthex(unsigned int n, char str)
+{
+	unsigned int	i;
+	int				count;
+	char			*base;
+
+	count = 0;
+	if (str == 'x')
+		base = "0123456789abcdef";
+	else
+		base = "0123456789ABCDEF";
+	i = 0;
+	if (n < 16)
+		count += ft_putchar(base[n]);
+	else
+	{
+		count += ft_puthex(n / 16, str);
+		count += ft_puthex(n % 16, str);
+	}
+	return (count);
+}
+
+int	ft_putunsigned(unsigned int nb)
+{
+	char	c;
+	int		count;
+
+	count = 0;
+	if (nb > 9)
+	{
+		count += ft_putunsigned(nb / 10);
+		count += ft_putunsigned(nb % 10);
+	}
+	else
+	{
+		c = nb + 48;
+		count += write(1, &c, 1);
+	}
+	return (count);
+}
+
+int	ft_pointer(unsigned long long n)
+{
+	int	count;
+
+	count = 0;
+	if (n < 16)
+		count += ft_puthex(n, 'x');
+	else
+	{
+		count += ft_pointer(n / 16);
+		count += ft_pointer(n % 16);
+	}
+	return (count);
+}
